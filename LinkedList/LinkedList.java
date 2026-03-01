efada6ef77decc26ae4117d699ff1fa27b53077e
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-03-01 21:09:48 +0530

    Practicing Linked List questions

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,19 @@
+package LinkedList;
+
+public class LinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    static void main() {
+
+    }
+}
