d9adf4390550241e17ce29e25eb03a57c04c250e
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-03-03 22:56:24 +0530

    Practicing Linked List questions

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -13,12 +13,14 @@ public class LinkedList {
 
     public static Node head;
     public static Node tail;
+    public static int size = 0;
 
-    // METHODS
+      // ************* METHODS **************
 
     //    1. Add Node at First of the LinkedList
     public void addFirst(int data) {
         Node newNode = new Node(data);
+        size++;
 
         if (head == null) {
             head = tail = newNode;
@@ -32,6 +34,8 @@ public class LinkedList {
     // 2. Add Node at the last of the LinkedList
     public void addLast(int data) {
         Node newNode = new Node(data);
+        size++;
+
         if (head == null) {
             head = tail = newNode;
             return;
@@ -41,8 +45,27 @@ public class LinkedList {
         tail = newNode;
     }
 
-    // Add in the middle
+    // 3. Add in the middle
+    public void add(int idx, int data) {
+        Node newNode = new Node(data);
+        size++;
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        int i = 0;
+        Node temp = head;
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
     //  Size of the LinkedList
+
     //  Remove first Node of the LinkedList
     //  Remove last Node of the LinkedList
     //  Search a key in the LinkedList (Iterative)
@@ -55,7 +78,7 @@ public class LinkedList {
     public void printLinkedList() {
         Node temp = head;
         while (temp != null) {
-            System.out.print(temp.data + " -> ");
+            System.out.print(STR."\{temp.data} -> ");
             temp = temp.next;
         }
         System.out.println("null");
@@ -78,8 +101,10 @@ public class LinkedList {
         ll.addLast(9);
         ll.addLast(10);
 
+        ll.add(3, 99);
+
         ll.printLinkedList();
 
-
+        System.out.println(STR."Size of the LinkedList is: \{size}");
     }
 }
