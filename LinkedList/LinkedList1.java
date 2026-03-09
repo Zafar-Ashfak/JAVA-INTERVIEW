dc40f8477ac74e2564700e3f86ae57dc2e5dccb2
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-03-09 20:37:21 +0530

    Practicing Linked List questions

--- LinkedList/LinkedList1.java
+++ LinkedList/LinkedList1.java
@@ -10,6 +10,7 @@ public class LinkedList1 {
             this.next = null;
         }
     }
+
     public static Node head;
     public static Node tail;
     public static int size;
@@ -17,6 +18,7 @@ public class LinkedList1 {
     public void addLast(int data) {
         Node newNode = new Node(data);
         size++;
+
         if (head == null) {
             head = tail = newNode;
             return;
@@ -26,10 +28,53 @@ public class LinkedList1 {
         tail = newNode;
     }
 
-    public boolean isPalindrome() {
-        // Find the middle node
+    public Node findMidNode(Node head) {
+        Node slow = head;
+        Node fast = head;
 
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
     }
+
+    public boolean isPalindrome() {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        // Find the middle node
+        Node mid = findMidNode(head);
+
+        // Divide the LinkedList in two halves and reverse the 2nd half (right)
+        Node prev = null;
+        Node curr = mid;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node left = head;
+        Node right = prev;
+
+        // Check both LinkedList are palindrome
+        while (right != null) {
+            if (left.data != right.data) {
+                return false;
+            }
+            left = left.next;
+            right  = right.next;
+        }
+
+        return true;
+    }
+
     static void main() {
         LinkedList1 ll = new LinkedList1();
         ll.addLast(1);
@@ -37,5 +82,12 @@ public class LinkedList1 {
         ll.addLast(3);
         ll.addLast(2);
         ll.addLast(1);
+
+        boolean res = ll.isPalindrome();
+        if (res) {
+            System.out.println("LinkedList is palindrome");
+        } else {
+            System.out.println("LinkedList is not palindrome");
+        }
     }
 }
