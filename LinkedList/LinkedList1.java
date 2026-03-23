cd001b671d0b21449149e0cb6e826cc69b80aeea
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-03-23 12:21:14 +0530

    Practice DSA with Java

--- LinkedList/LinkedList1.java
+++ LinkedList/LinkedList1.java
@@ -40,6 +40,40 @@ public class LinkedList1 {
         return slow;
     }
 
+    public boolean isPalindrome() {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        // Find the middle node
+        Node mid = findMidNode(head);
+
+        // Divide the LinkedList in two halves and reverse the 2nd half (right)
+        Node prev = null;
+        Node curr = mid;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node left = head;
+        Node right = prev;
+
+        // Check both LinkedList are palindrome
+        while (right != null) {
+            if (left.data != right.data) {
+                return false;
+            }
+            left = left.next;
+            right  = right.next;
+        }
+
+        return true;
+    }
 
     static void main() {
         LinkedList1 ll = new LinkedList1();
@@ -49,11 +83,11 @@ public class LinkedList1 {
         ll.addLast(2);
         ll.addLast(1);
 
-//        boolean res = ll.isPalindrome();
-//        if (res) {
-//            System.out.println("LinkedList is palindrome");
-//        } else {
-//            System.out.println("LinkedList is not palindrome");
-//        }
+        boolean res = ll.isPalindrome();
+        if (res) {
+            System.out.println("LinkedList is palindrome");
+        } else {
+            System.out.println("LinkedList is not palindrome");
+        }
     }
 }
